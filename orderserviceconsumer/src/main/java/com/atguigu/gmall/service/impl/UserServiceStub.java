84ea5e155b85e3ad4f0ae1e970ac38a6eacf4d04
htdingkang <ht_dingkang@126.com>
2019-10-06 23:27:56 +0800

    本地存根

---
+++ orderserviceconsumer/src/main/java/com/atguigu/gmall/service/impl/UserServiceStub.java
@@ -0,0 +1,29 @@
+package com.atguigu.gmall.service.impl;
+
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.service.UserService;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+public class UserServiceStub implements UserService {
+
+    private final UserService userService;
+
+    /**
+     * 传入userService远程代理对象
+     * @param userService
+     */
+    public UserServiceStub(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public List<UserAddress> getUserAddressList(String userId) {
+        System.out.println("userServiceStub...");
+        if(!StringUtils.isEmpty(userId)){
+            return userService.getUserAddressList(userId);
+        }
+        return null;
+    }
+}
