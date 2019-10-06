ae8441570a638cdf77f022d58abda4691499dec2
htdingkang <ht_dingkang@126.com>
2019-10-06 16:53:17 +0800

    启动检查

--- orderserviceconsumer/src/main/java/com/atguigu/gmall/MainApplication.java
+++ orderserviceconsumer/src/main/java/com/atguigu/gmall/MainApplication.java
@@ -10,8 +10,8 @@ public class MainApplication {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext ioc =
                 new ClassPathXmlApplicationContext("consumer.xml");
-        OrderService bean = ioc.getBean(OrderService.class);
-        bean.initOrder("1");
+//        OrderService bean = ioc.getBean(OrderService.class);
+//        bean.initOrder("1");
         System.out.println("调用完成");
         System.in.read();
     }
