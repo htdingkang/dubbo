2ac62cfca03f3cf83196a289ab531964f1a6c38f
htdingkang <ht_dingkang@126.com>
2019-10-06 18:23:08 +0800

    多版本

--- userserviceprovider/src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl2.java
+++ userserviceprovider/src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl2.java
@@ -7,12 +7,11 @@ import java.util.Arrays;
 import java.util.List;
 
 
-
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl2 implements UserService {
 
 	@Override
 	public List<UserAddress> getUserAddressList(String userId) {
-		System.out.println("getUserAddressList...old...");
+		System.out.println("getUserAddressList...new...");
 		UserAddress address1 = new UserAddress(1, "北京市昌平区宏福科技园综合楼3层", "1", "李老师", "010-56253825", "Y");
 		UserAddress address2 = new UserAddress(2, "深圳市宝安区西部硅谷大厦B座3层（深圳分校）", "1", "王老师", "010-56253825", "N");
 		try {
